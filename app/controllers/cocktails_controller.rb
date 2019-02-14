ccfcbf3186840459c5a5d420e10ff844290a7679
Nicolas Eymenier <nicolas.eymenier@edhec.com>
2019-02-14 18:56:41 +0100

    doses pending

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
   def new
     @cocktail = Cocktail.new
   end
@@ -8,17 +9,26 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def edit
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
+    redirect_to cocktail_path(@cocktail)
   end
+
 
   private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,29 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+  def new
+    # we need @restaurant in our `simple_form_for`
+
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `cocktail_id` to asssociate dose with corresponding cocktail
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
