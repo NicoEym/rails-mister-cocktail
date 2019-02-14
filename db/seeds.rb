459d99fdfc3304a980e43c25c020c875a2d0d09d
Nicolas Eymenier <nicolas.eymenier@edhec.com>
2019-02-14 16:05:34 +0100

    index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = open(url).read
+ingredient = JSON.parse(ingredients)
+list_of_ingredient = ingredient['drinks']
+
+list_of_ingredient.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
