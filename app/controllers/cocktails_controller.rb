97bb56e35b72ca16186360e08562cf5e0ccc97a6
Nicolas Eymenier <nicolas.eymenier@edhec.com>
2019-02-15 12:31:57 +0100

    ok delete cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,6 +17,12 @@ class CocktailsController < ApplicationController
     redirect_to cocktail_path(@cocktail)
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
 
   private
 
