dc7c049922c1e897470c853a95616f912d66b97a
Nicolas Eymenier <nicolas.eymenier@edhec.com>
2019-02-15 12:26:57 +0100

    ok delete dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,21 +1,12 @@
 class DosesController < ApplicationController
   before_action :set_cocktail, only: [:new, :create]
   def new
-    # we need @restaurant in our `simple_form_for`
-
+    # we need @restaurant in our `simple_form_for
     @dose = Dose.new
   end
 
-  def create
-    # @dose = Dose.new(dose_params)
-    # # we need `cocktail_id` to asssociate dose with corresponding cocktail
-    # @dose.cocktail = @cocktail
-    # if @dose.save
-    #   redirect_to cocktail_path(@cocktail)
-    # else
-    #   render :new
-    # end
 
+  def create
     @ingredients = Ingredient.find(params[:dose][:ingredient_id])
     @ingredients.each do |ingredient|
       dose = Dose.new
@@ -25,6 +16,13 @@ class DosesController < ApplicationController
       dose.save
     end
     redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:new, :index, :create, :show] do
     resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
